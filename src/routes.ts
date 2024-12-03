250389ea554f5f703f2d12e9d47bc3d7f6a28c70
salvador <salvador.martinez.ksmg@gmail.com>
2024-12-03 13:20:26 -0600

    deploy setup

--- src/routes.ts
+++ src/routes.ts
@@ -3,7 +3,6 @@ import { helloWorld } from "./controller/hwlloWorld.controller";
 
 const router = Router();
 
-export const routes = () => {
-  router.get("/helloWorld", helloWorld);
-};
+router.get("/helloWorld", helloWorld);
+
 export default router;
