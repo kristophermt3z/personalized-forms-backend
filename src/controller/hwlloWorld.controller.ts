1faa85f582e9840aef8f6eed7e17f14a947dc355
salvador <salvador.martinez.ksmg@gmail.com>
2024-12-10 10:47:14 -0600

    home message

--- src/controller/hwlloWorld.controller.ts
+++ src/controller/hwlloWorld.controller.ts
@@ -1,15 +1,11 @@
 import { Request, Response } from "express";
 
-export const helloWorld = async (
-  req: Request,
-  res: Response
-) => {
+export const helloWorld = async (req: Request, res: Response) => {
   try {
     return res.json({
-      "success": true,
-      "message": "Hello, lets create forms :)"
-    }
-    );
+      success: true,
+      message: "Empower your creativity with dynamic forms.",
+    });
   } catch (error) {
     console.log(error);
     return res.status(500).json({
