10c2f7ea433ff11d505737c0c9bf1d4dcd225e4c
salvador <salvador.martinez.ksmg@gmail.com>
2024-12-16 14:29:32 -0600

    view by id + update

--- src/controller/form.controller.ts
+++ src/controller/form.controller.ts
@@ -30,3 +30,45 @@ export const getForms = async (req: Request, res: Response) => {
     res.status(500).json({ message: "Error fetching forms.", error });
   }
 };
+
+export const getFormById = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+    const form = await Form.findById(id);
+
+    if (!form) {
+      return res.status(404).json({ message: "Form not found" });
+    }
+    
+    res.status(200).json(form);
+  } catch (error) {
+    console.error("Error fetching form:", error);
+    res.status(500).json({ message: "Error fetching form.", error });
+  }
+};
+
+export const updateForm = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+    const { title, description, fields } = req.body;
+
+    if (!title || !description || !fields) {
+      return res.status(400).json({ message: "All fields are required." });
+    }
+
+    const updatedForm = await Form.findByIdAndUpdate(
+      id,
+      { title, description, fields },
+      { new: true }
+    );
+
+    if (!updatedForm) {
+      return res.status(404).json({ message: "Form not found" });
+    }
+
+    res.status(200).json({ message: "Form updated successfully.", form: updatedForm });
+  } catch (error) {
+    console.error("Error updating form:", error);
+    res.status(500).json({ message: "Error updating form.", error });
+  }
+};
--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,15 +1,19 @@
 import { Router } from "express";
 import { helloWorld } from "../controller/helloWorld.controller";
 import { login, register } from "../controller/auth.controller";
-import { createForm, getForms } from "../controller/form.controller";
+import { createForm, getFormById, getForms, updateForm } from "../controller/form.controller";
 import { authenticate } from "../middlewares/auth.middleware";
 
 const router = Router();
 
 router.get("/helloWorld", helloWorld);
+
 router.post("/auth/register", register);
 router.post("/auth/login", login);
+
 router.post("/forms/create-form", authenticate, createForm);
 router.get("/forms/get-forms", authenticate, getForms);
+router.get("/forms/:id", authenticate, getFormById);
+router.put("/forms/:id", authenticate, updateForm);
 
 export default router;
